0dc1e63d1d1e2d3db029f651c39298dd179493f4
leonGravel <la9249@163.com>
2018-07-15 00:04:35 +0800

    :tada: 反转整数

--- src/PalindromeNumber.java
+++ src/PalindromeNumber.java
@@ -1,9 +1,28 @@
 /**
  * Created by leebr on 2018/7/14.
+ *
+ * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+
+ * 示例 1:
+ * 输入: 121
+ * 输出: true
+
+ * 示例 2:
+ * 输入: -121
+ * 输出: false
+ * 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+
+ * 示例 3:
+ * 输入: 10
+ * 输出: false
+ * 解释: 从右向左读, 为 01 。因此它不是一个回文数。
  */
 public class PalindromeNumber {
     public boolean isPalindrome(int x) {
         return String.valueOf(x).equals(new StringBuffer().append(x).reverse().toString());
+    }
+    public static void main(String[] args) throws Exception {
+        System.out.println(new PalindromeNumber().isPalindrome(121));
     }
 }
 
---
+++ src/ReverseInteger.java
@@ -0,0 +1,36 @@
+/**
+ * Created by leebr on 2018/7/15.
+ *
+ * 给定一个 32 位有符号整数，将整数中的数字进行反转。
+
+ * 示例 1:
+ * 输入: 123
+ * 输出: 321
+
+ * 示例 2:
+ * 输入: -123
+ * 输出: -321
+
+ * 示例 3:
+ * 输入: 120
+ * 输出: 21
+ */
+public class ReverseInteger {
+    public int reverse(int x) {
+        StringBuffer s = new StringBuffer().append(x);
+        String newNum = "";
+        if (x < 0) {
+            newNum = new StringBuffer(s.reverse()).substring(0, s.length() - 1);
+        } else {
+            newNum = new StringBuffer(s.reverse()).toString();
+        }
+        try {
+            return (x < 0) ? -Integer.valueOf(newNum) : Integer.valueOf(newNum);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        System.out.println(new ReverseInteger().reverse(121));
+    }
+}
