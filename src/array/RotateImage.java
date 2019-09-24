2cb5704f6438d8be2d7b71590b602e647ed10899
Ao Li <la9249@163.com>
2019-09-24 23:08:29 +0800

    Array: 旋转图像

---
+++ src/array/RotateImage.java
@@ -0,0 +1,74 @@
+package array;
+
+/**
+ * @author Gravel
+ * @date 2019/9/18.
+ * <p>
+ * 给定一个 n × n 的二维矩阵表示一个图像。
+ * <p>
+ * 将图像顺时针旋转 90 度。
+ * <p>
+ * 说明：
+ * <p>
+ * 你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 给定 matrix =
+ * [
+ * [1,2,3],
+ * [4,5,6],
+ * [7,8,9]
+ * ],
+ * <p>
+ * 原地旋转输入矩阵，使其变为:
+ * [
+ * [7,4,1],
+ * [8,5,2],
+ * [9,6,3]
+ * ]
+ * 示例 2:
+ * <p>
+ * 给定 matrix =
+ * [
+ * [ 5, 1, 9,11],
+ * [ 2, 4, 8,10],
+ * [13, 3, 6, 7],
+ * [15,14,12,16]
+ * ],
+ * <p>
+ * 原地旋转输入矩阵，使其变为:
+ * [
+ * [15,13, 2, 5],
+ * [14, 3, 4, 1],
+ * [12, 6, 8, 9],
+ * [16, 7,10,11]
+ * ]
+ * <p>
+ * 思路1： 先对角翻转 然后在上下翻转
+ */
+public class RotateImage {
+    public void rotateImage(int[][] matrix) {
+        // 对角翻转
+        int len = matrix.length - 1;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length && j != (len - i); j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[len - j][len - i];
+                matrix[len - j][len - i] = temp;
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length / 2; j++) {
+                int temp = matrix[j][i];
+                matrix[j][i] = matrix[len - j][i];
+                matrix[len - j][i] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int[][] arr = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+        new RotateImage().rotateImage(arr);
+    }
+}
