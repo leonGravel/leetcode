3832872f8e3e941927a303e2bdc2f7c85f621db6
leongravel <leebroncc@gmail.com>
2019-11-10 14:58:02 +0800

    [update] 修改删除第n个节点的遍历方式，为一次遍历

--- linkedlist/RemoveNthFromEnd.java
+++ linkedlist/RemoveNthFromEnd.java
@@ -3,52 +3,54 @@ package linkedlist;
 /**
  * @author Gravel
  * @date 2019/10/16.
- *
+ * <p>
  * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
- *
+ * <p>
  * 示例：
- *
+ * <p>
  * 给定一个链表: 1->2->3->4->5, 和 n = 2.
- *
+ * <p>
  * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
  * 说明：
- *
+ * <p>
  * 给定的 n 保证是有效的。
+ *
+ * 思路：用两个指针，第一个指针先走 n-1步，然后第二个指针开始走，当第一个指针走到末尾的时候，第二个指针所在的
+ * 位置就是第n个节点，然后删掉他，需要考虑首尾节点删除的问题
  */
 public class RemoveNthFromEnd {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-
         int index = 0;
-        for (ListNode temp = head; temp!=null;temp = temp.next){
+        int deleteindex = n;
+        boolean isLastNode = false;
+
+        if(deleteindex==1){
+            isLastNode = true;
+            deleteindex += 1;
+        }
+        ListNode secendNode = head;
+        for (ListNode temp = head; temp != null; temp = temp.next) {
+            if (index >= deleteindex) {
+                secendNode = secendNode.next;
+            }
+
             index++;
         }
-        if(index == n){
+        if (index == n) {
             return head.next;
         }
-        int deleteIndex = 0;
-        if(n==1){
-            for (ListNode temp = head; temp!=null;temp = temp.next){
-                if(deleteIndex ==(index-n-1) ){
-                    temp.next = null;
-                    return head;
-                }
-                deleteIndex++;
-            }
+        if(isLastNode){
+            secendNode.next = null;
+            return head;
         }
-        deleteIndex = 0;
-        for (ListNode temp = head; temp!=null;temp = temp.next){
-            deleteIndex++;
-            if(deleteIndex ==(index-n+1) ){
-                temp.val = temp.next.val;
-                temp.next = temp.next.next;
-                break;
-            }
-        }
+        secendNode.val = secendNode.next.val;
+        secendNode.next = secendNode.next.next;
         return head;
     }
+
     public static void main(String[] args) {
         ListNode a = new ListNode(1);
         a.next = new ListNode(2);
-        new RemoveNthFromEnd().removeNthFromEnd(a,1);
+        new RemoveNthFromEnd().removeNthFromEnd(a, 1);
     }
 }
