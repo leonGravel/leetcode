1c29245814c5b421b4d82a860913d9825aa0af07
leongravel <leebroncc@gmail.com>
2019-12-19 23:26:46 +0800

    [add] 打印二叉树的右视图

---
+++ trees/RightSideView.java
@@ -0,0 +1,45 @@
+package trees;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @ClassName RightSideView
+ * @Description: 二叉树的右视图
+ * @Author gravel
+ * @Date 2019/12/19
+ * @Version V1.0
+ * 思路：
+ * 宽度优先遍历，打印每一层的第一个数字
+ **/
+
+public class RightSideView {
+    public List<Integer> rightSideView(TreeNode root) {
+        if(root==null) {
+            return new ArrayList<>();
+        }
+        List<Integer> result = new ArrayList<>();
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.push(root);
+        int len = 1;
+        while (!queue.isEmpty()){
+            int j = 0;
+            for(int i=0;i<len;i++){
+                TreeNode cur = queue.poll();
+                if(cur!=null){
+                    queue.add(cur.right);
+                    queue.add(cur.left);
+                    j++;
+                }
+                // 每层只取第一个数字
+                if(j==1){
+                    result.add(cur.val);
+                }
+            }
+            len = queue.size();
+        }
+        return result;
+    }
+
+}
